9ba6c9bf4540ed123b9aa8500cb3c6fffbd7d726
Julius Välimaa <julius.valimaa@gmail.com>
2020-02-10 23:39:09 +0200

    Debug script

--- app/static/script.js
+++ app/static/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('input[type=radio][name=cruise]').change(function() {
+    $('input[type=checkbox][name=cruise]').change(function() {
         if (document.getElementById('cruise').checked) {
             $('#buffet').show()
         } else {
