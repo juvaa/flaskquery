f1e5bd86d5d16929aa3ea33e1590b264a33cbd00
Julius Välimaa <julius.valimaa@gmail.com>
2020-02-12 08:46:11 +0200

    Add length validators

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, RadioField, SelectField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, InputRequired, Length
 
 class Form(FlaskForm):
-    name = StringField('Nimi*', validators=[DataRequired()])
+    name = StringField('Nimi*', validators=[DataRequired(), Length(max=64)])
     guild = RadioField('Kilta*', choices=(['otit', 'OTiT'],['sik', 'SIK']))
-    mail = StringField('Sähköpostiosoite*', validators=[DataRequired()])
-    phone = StringField('Puhelinnumero*', validators=[DataRequired()])
+    mail = StringField('Sähköpostiosoite*', validators=[DataRequired(), Length      (max=64)])
+    phone = StringField('Puhelinnumero*', validators=[DataRequired(), Length        (max=20)])
     place = SelectField('Lähtöpaikka*', choices=(['yliopisto', 'Yliopisto'], ['tuira', 'Tuira'], ['linja-autoasema', 'Linja-autoasema']), validators=[DataRequired()])
     cruise = BooleanField('Lähden risteilylle')
     buffet = BooleanField('Haluan buffetin')
