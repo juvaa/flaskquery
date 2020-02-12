d02d6487c29fb3bf5192b7279b5f6f2a0f3349cc
Julius Välimaa <julius.valimaa@gmail.com>
2020-02-12 10:27:04 +0200

    Add validator

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, InputRequired, Optional, Length
 
 class Form(FlaskForm):
     name = StringField('Nimi*', validators=[DataRequired(), Length(max=64)])
-    guild = RadioField('Kilta*', choices=(['otit', 'OTiT'],['sik', 'SIK']))
+    guild = RadioField('Kilta*', choices=(['otit', 'OTiT'],['sik', 'SIK']),         validators=[DataRequired()])
     mail = StringField('Sähköpostiosoite*', validators=[DataRequired(), Length      (max=64)])
     phone = StringField('Puhelinnumero*', validators=[DataRequired(), Length        (max=20)])
     place = SelectField('Lähtöpaikka*', choices=(['yliopisto', 'Yliopisto'], ['tuira', 'Tuira'], ['linja-autoasema', 'Linja-autoasema']), validators=[DataRequired()])
