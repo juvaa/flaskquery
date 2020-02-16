f34e947ae975b879a759379e612770d38ae722a9
Julius Välimaa <julius.valimaa@gmail.com>
2020-02-16 23:01:15 +0200

    Add visibility toggle to sitsit related fields

--- app/static/script.js
+++ app/static/script.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $('input[type=checkbox][name=cruise]').change(function() {
         $('#buffet').toggle()
     });
+    $('input[type=checkbox][name=sitsit]').change(function() {
+        $('#alcohol').toggle()
+        $('#specialneeds').toggle()
+    });
 })
