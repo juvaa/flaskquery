f38b5fe24f2c0296ad904365d2b446a750117113
Julius Välimaa <julius.valimaa@gmail.com>
2020-03-05 01:04:11 +0200

    Add lenght validation

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, RadioField, SelectField, TextAreaField
-from wtforms.validators import DataRequired, InputRequired, Optional
+from wtforms.validators import DataRequired, InputRequired, Optional, Length
 
 class Form(FlaskForm):
-    name = StringField('Nimi*', validators=[DataRequired()])
+    name = StringField('Nimi*', validators=[DataRequired(), Length(max=64)])
     guild = RadioField('Ryhmä*', choices=(['otit', 'OTiT'],['sik', 'SIK'], ['blanko', 'Blanko'], ['henkilokunta', 'Tiedekunnan henkilöstö']))
-    mail = StringField('Sähköpostiosoite*', validators=[DataRequired()])
-    specialfoods = TextAreaField('Erityisruokavaliot (Vastaa vaikket osallistuisi sitseille)')
-    hopesndreams = TextAreaField('Toiveita seminaarissa käsiteltävistä asioista.')
+    mail = StringField('Sähköpostiosoite*', validators=[DataRequired(), Length(max=64)])
+    specialfoods = TextAreaField('Erityisruokavaliot (Vastaa vaikket osallistuisi sitseille)', validators=[Length(max=500)])
+    hopesndreams = TextAreaField('Toiveita seminaarissa käsiteltävistä asioista', validators=[Length(max=500)])
     attend = BooleanField('Kyllä')
     alcohol = RadioField('Juomatoive', choices=(['alkoholillinen', 'Alkoholillinen'], ['alkoholiton', 'Alkoholiton']), validators=[Optional()])
     wine = RadioField('Juomatoive', choices=(['punaviini', 'Punaviini'], ['valkoviini', 'Valkoviini']), validators=[Optional()])
