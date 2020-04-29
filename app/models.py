03807638887d1bc4cfb3bcaed3c532399672e984
Julius Välimaa <julius.valimaa@gmail.com>
2020-04-29 14:54:09 +0300

    Reformat db and db calls

--- app/models.py
+++ app/models.py
@@ -1,10 +1,25 @@
 from app import db
 
-class Model(db.Model):
+class Hallitus(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    hallitus = db.Column(db.String(500))
-    tapahtuma = db.Column(db.String(500))
-    ehdotus = db.Column(db.String(500))
-    muuta = db.Column(db.String(500))
+    palaute = db.Column(db.String(500))
+    datetime = db.Column(db.DateTime())
+    arkisto = db.Column(db.Boolean())
+
+class Tapahtuma(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    palaute = db.Column(db.String(500))
+    datetime = db.Column(db.DateTime())
+    arkisto = db.Column(db.Boolean())
+
+class Ehdotus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    palaute = db.Column(db.String(500))
+    datetime = db.Column(db.DateTime())
+    arkisto = db.Column(db.Boolean())
+
+class Muuta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    palaute = db.Column(db.String(500))
     datetime = db.Column(db.DateTime())
     arkisto = db.Column(db.Boolean())
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, request
 from app import app, db
 from app.forms import Form
-from app.models import Model
+from app.models import Hallitus, Tapahtuma, Ehdotus, Muuta
 from datetime import datetime
 from flask_basicauth import BasicAuth
 from os import environ
@@ -21,15 +21,30 @@ def index():
 
     if form.validate_on_submit():
         flash('Thank you for participating')
-        sub = Model(
-            hallitus = form.hallitus.data,
-            tapahtuma = form.tapahtuma.data,
-            ehdotus = form.ehdotus.data,
-            muuta = form.muuta.data,
+        h_sub = Hallitus(
+            palaute = form.hallitus.data,
             datetime = nowtime,
             arkisto = False
         )
-        db.session.add(sub)
+        t_sub = Tapahtuma(
+            palaute = form.tapahtuma.data,
+            datetime = nowtime,
+            arkisto = False
+        )
+        e_sub = Ehdotus(
+            palaute = form.ehdotus.data,
+            datetime = nowtime,
+            arkisto = False
+        )
+        m_sub = Muuta(
+            palaute = form.muuta.data,
+            datetime = nowtime,
+            arkisto = False
+        )
+        db.session.add(h_sub)
+        db.session.add(t_sub)
+        db.session.add(e_sub)
+        db.session.add(m_sub)
         db.session.commit()
         return redirect(appurl)
     return render_template('index.html', title='Palautetta OTiT:lle',
@@ -40,51 +55,50 @@ def index():
 @app.route('/admin', methods=['GET', 'POST'])
 @basic_auth.required
 def admin():
-    h_entries = Model.query.with_entities(Model.hallitus)
-    t_entries = Model.query.with_entities(Model.tapahtuma)
-    e_entries = Model.query.with_entities(Model.ehdotus)
-    m_entries = Model.query.with_entities(Model.muuta)
-    count = Model.query.count()
-    entries = Model.query.all()
+    h_entries = Hallitus.query.all()
+    t_entries = Tapahtuma.query.all()
+    e_entries = Ehdotus.query.all()
+    m_entries = Muuta.query.all()
+    h_new = Hallitus.query.filter_by(arkisto = False).count()
+    t_new = Tapahtuma.query.filter_by(arkisto = False).count()
+    e_new = Ehdotus.query.filter_by(arkisto = False).count()
+    m_new = Muuta.query.filter_by(arkisto = False).count()
+    new = h_new + t_new + e_new + m_new
 
     if request.form:
 
         for h_entry in h_entries:
-            if str(h_entry.id) in request.form.keys():
-                if request.form[str(h_entry.id)] == "arkisto":
+            if ("h_" + str(h_entry.id)) in request.form.keys():
+                if request.form[("h_" + str(h_entry.id))] == "arkisto":
                     h_entry.arkisto = True
                     flash("Palaute arkistoitu")
-                elif request.form[str(h_entry.id)] == "remove":
-                    h_entry.hallitus = ""
+                elif request.form[("h_" + str(h_entry.id))] == "remove":
+                    db.session.delete(h_entry)
                     flash("Palaute poistettu")
         for t_entry in t_entries:
-            if str(t_entry.id) in request.form.keys():
-                if request.form[str(t_entry.id)] == "arkisto":
+            if ("t_" + str(t_entry.id)) in request.form.keys():
+                if request.form[("t_" + str(t_entry.id))] == "arkisto":
                     t_entry.arkisto = True
                     flash("Palaute arkistoitu")
-                elif request.form[str(t_entry.id)] == "remove":
-                    t_entry.tapahtuma = ""
+                elif request.form[("t_" + str(t_entry.id))] == "remove":
+                    db.session.delete(t_entry)
                     flash("Palaute poistettu")
         for e_entry in e_entries:
-            if str(e_entry.id) in request.form.keys():
-                if request.form[str(e_entry.id)] == "arkisto":
+            if ("e_" + str(e_entry.id)) in request.form.keys():
+                if request.form[("e_" + str(e_entry.id))] == "arkisto":
                     e_entry.arkisto = True
                     flash("Palaute arkistoitu")
-                elif request.form[str(e_entry.id)] == "remove":
-                    e_entry.ehdotus = ""
+                elif request.form[("e_" + str(e_entry.id))] == "remove":
+                    db.session.delete(e_entry)
                     flash("Palaute poistettu")
         for m_entry in m_entries:
-            if str(m_entry.id) in request.form.keys():
-                if request.form[str(m_entry.id)] == "arkisto":
+            if ("m_" + str(m_entry.id)) in request.form.keys():
+                if request.form[("m_" + str(m_entry.id))] == "arkisto":
                     m_entry.arkisto = True
                     flash("Palaute arkistoitu")
-                elif request.form[str(m_entry.id)] == "remove":
-                    m_entry.muuta = ""
+                elif request.form[("m_" + str(m_entry.id))] == "remove":
+                    db.session.delete(m_entry)
                     flash("Palaute poistettu")
-        for entry in entries:
-            if (entry.hallitus and entry.tapahtuma and entry.ehdotus
-                and entry.muuta):
-                db.session.delete(entry)
         db.session.commit()
         return redirect(appurl + '/admin')
     return render_template('admin.html', title='Palaute boxi',
@@ -92,5 +106,5 @@ def admin():
                                          t_entries=t_entries,
                                          e_entries=e_entries,
                                          m_entries=m_entries,
-                                         count=count,
+                                         new=new,
                                          appurl=appurl)
