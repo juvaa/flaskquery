d40024520d046c984cd79e3b1e4b3942518b01ee
Julius Välimaa <julius.valimaa@gmail.com>
2020-04-30 18:30:33 +0300

    Add overflow mitigation

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, RadioField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class Form(FlaskForm):
-    hallitus = TextAreaField('Palautetta hallitukselle toiminnasta:')
-    tapahtuma = TextAreaField('Palautetta tapahtumasta:')
+    hallitus = TextAreaField('Palautetta hallitukselle toiminnasta:', validators=[Length(max=500)])
+    tapahtuma = TextAreaField('Palautetta tapahtumasta:', validators=[Length(max=500)])
     ehdotus = TextAreaField(
-        'Ehdotuksia tapahtumasta/aktiviteetista/kiltahuonehankinnasta:')
-    muuta = TextAreaField('Muuta, mitä haluat hallituksen tietävän:')
+        'Ehdotuksia tapahtumasta/aktiviteetista/kiltahuonehankinnasta:', validators=[Length(max=500)])
+    muuta = TextAreaField('Muuta, mitä haluat hallituksen tietävän:', validators=[Length(max=500)])
     submit = SubmitField('Lähetä')
